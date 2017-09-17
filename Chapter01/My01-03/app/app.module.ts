1b782c2a1a3cc56503735bb6c9d0434d7769f5ec
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-09-17 17:50:49 -0400

    Add ContentFilterPipe to filter dives based on sub string

--- Chapter01/My01-03/app/app.module.ts
+++ Chapter01/My01-03/app/app.module.ts
@@ -1,13 +1,17 @@
 import {NgModule}        from "@angular/core";
 import {BrowserModule}    from "@angular/platform-browser";
 
-
-import { DiveLogComponent}    from "./dive-log.component";
+//business logic
+import { DiveLogComponent }    from "./dive-log.component";
+import { ContentFilterPipe }    from "./content-filter.pipe";
 
 @NgModule({
     imports: [BrowserModule]
-    ,declarations: [DiveLogComponent]
-    ,bootstrap: [DiveLogComponent]
+    ,declarations: [//of what's unique about this app
+        DiveLogComponent
+        ,ContentFilterPipe
+    ]
+    ,bootstrap: [DiveLogComponent]//which part is the base/root/bootstrap for the rest
 })
 
 export class AppModule{
