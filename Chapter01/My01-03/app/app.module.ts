19d6d044a301076c6fb0a40ed99ccc7152e4bf62
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-09-17 20:12:25 -0400

    Add DiveFilterPipe as a renamed clone of ContentFilterPipe in order to preserve the naming conventions. Import new DiveFilterPipe clone in AppModule (even though it's duplicate code).

--- Chapter01/My01-03/app/app.module.ts
+++ Chapter01/My01-03/app/app.module.ts
@@ -4,12 +4,14 @@ import {BrowserModule}    from "@angular/platform-browser";
 //business logic
 import { DiveLogComponent }    from "./dive-log.component";
 import { ContentFilterPipe }    from "./content-filter.pipe";
+import { DiveFilterPipe }      from "./dive-filter.pipe";
 
 @NgModule({
     imports: [BrowserModule]
     ,declarations: [//of what's unique about this app
         DiveLogComponent
         ,ContentFilterPipe
+        ,DiveFilterPipe
     ]
     ,bootstrap: [DiveLogComponent]//which part is the base/root/bootstrap for the rest
 })
