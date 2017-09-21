345d9bc71015f84542b0e49737b5fe6b19cd48f3
Todd Mullen <Todd Mullen>
2017-09-21 19:47:56 -0400

    Add partial implementation for adding a new DiveSite. (so I can switch to my full laptop ;) )

--- Chapter02/My02-01/app/dive-app.component.ts
+++ Chapter02/My02-01/app/dive-app.component.ts
@@ -11,12 +11,25 @@ export class DiveAppComponent{
 		LIST: "list"
 		,ADD: "add"
 	}
-	siteAry = DiveSite.BestSites;
-	currentView = this.Views.LIST;
+	siteAry:DiveSite[]	= DiveSite.BestSites;
+	currentView:string	= this.Views.LIST;
+	newSiteId:number;
 
 	navigateTo(view:string):void{
 		console.log("DiveAppComponent::navigateTo()", view);
 		this.currentView = view;
+	}
+
+	addSite(){
+		this.newSiteId = this.siteAry
+							.map( s => s.id )
+							.reduce( (p,c) => p < c ? c : p ) + 1;
+		this.navigateTo( this.Views.ADD );
+	}//addSite()
+
+	siteAdded( newSiteName:string ){
+		console.log("DiveAppComponent::siteAdded()", arguments );
+		this.siteAry.push( <DiveSite>{id: this.newSiteId, name: newSiteName} );
 	}
 
 }//DiveAppComponent
