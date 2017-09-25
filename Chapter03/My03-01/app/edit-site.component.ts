b4071a8455e748d3827c07d067dd0eabebf37ee6
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-09-24 22:50:41 -0400

    EditSiteComponent,  add @input property setter to set/catch the id of the selected site and use that to look up the site to use.

--- Chapter03/My03-01/app/edit-site.component.ts
+++ Chapter03/My03-01/app/edit-site.component.ts
@@ -1,22 +1,49 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
-import {DiveSite} from './dive-site';
+
+import {SiteMgmtService}	from "./site-mgmt.service";
 
 @Component({
   selector: 'edit-site-view',
   templateUrl: 'app/edit-site.template.html'
 })
 export class EditSiteComponent {
-  @Input() site: DiveSite;
-  @Output() onSaved = new EventEmitter<DiveSite>();
-  @Output() onCancel = new EventEmitter();
+	@Output() onClosed = new EventEmitter();
 
-  saved() {
-    if (this.site.id) {
-      this.onSaved.emit(this.site);
-    }
-  }
+//	@Input() siteId:number;//this kind of binding can't work, we need to attack the value to an event
+	private _siteId:number;
+	@Input() set siteId( id:number ){
+		console.log("EditSiteComponent::setSiteId(): ", this.siteId );
+		this._siteId = id;
+		let site = this.siteService.getSiteById( this._siteId );
+		if( site ){
+			this.siteName = site.name;
+			this.maxDepth = site.maxDepth;
+		}
 
-  cancel() {
-    this.onCancel.emit(null);
-  }
-}
+	}
+	siteName:string;
+	maxDepth:number;
+
+	constructor( private siteService:SiteMgmtService ){
+//		console.log("EditSiteComponent() siteId: ", this.siteId );
+		//on construction, this.siteId is a semi-meaningless value from the list view
+		//it won't have a meaningful value until a runtime click event "picks" the siteId value
+		//so we attach a setter method to the @Input event & trigger the load when it's fired (as above)
+//		let site = this.siteService.getSiteById( this.siteId );
+//		if( site ){
+//			this.siteName = site.name;
+//			this.maxDepth = site.maxDepth;
+//		}
+	}
+
+	doSave():void{
+		this.siteService.saveSite( {id: this.siteId, name: this.siteName, maxDepth: this.maxDepth} );
+		this.onClosed.emit(null);
+	}
+
+	doCancel():void{
+		this.onClosed.emit(null);
+	}
+
+
+}//EditSiteComponent
