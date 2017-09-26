82f3524bb546ddcddc63581ac29ebbfdcd7fe581
Todd Mullen <ToddJMullen@gmail.com>
2017-09-25 21:29:52 -0400

    Add rotation to ActionableDirective (but it doesn't work =/ )

--- Chapter03/My03-01/app/actionable.directive.ts
+++ Chapter03/My03-01/app/actionable.directive.ts
@@ -10,19 +10,21 @@ export class ActionableDirective{
     }
 
     @HostListener('mouseenter') onMouseEnter(){
-        this.setAppearance( "#aaa", "pointer" );
+        this.setAppearance( "#aaa", "pointer", 7 );
     }
 
     @HostListener('mouseleave') onMouseLeave(){
-        this.setAppearance( null, null );
+        this.setAppearance( null, null, 0 );
     }
 
 
-    setAppearance( color:string, cursor:string ){
-//        console.log("ActionableDirective::setAppearance()", color, cursor );
+    setAppearance( color:string, cursor:string, degs ){
+        let rotation:string = "rotate(" + degs + "degs)";
+        console.log("ActionableDirective::setAppearance()", color, cursor, rotation );
         let style = this.element.nativeElement.style;
         style.backgroundColor = color;
         style.cursor = cursor;
+        style.transform = rotation;
     }
 
 }//ActionableDirective
