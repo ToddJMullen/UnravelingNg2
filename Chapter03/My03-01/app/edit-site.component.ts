78757e35f3d985f88221d8e09fff77c85b4112ff
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-09-26 20:50:34 -0400

    EditSiteComponent replace navigation methods with routing implementation

--- Chapter03/My03-01/app/edit-site.component.ts
+++ Chapter03/My03-01/app/edit-site.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+//import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component}	from "@angular/core";
+import {Router, ActivatedRoute}	from "@angular/router";
 
 import {SiteMgmtService}	from "./site-mgmt.service";
 
@@ -7,24 +9,35 @@ import {SiteMgmtService}	from "./site-mgmt.service";
   templateUrl: 'app/edit-site.template.html'
 })
 export class EditSiteComponent {
-	@Output() onClosed = new EventEmitter();
+//	@Output() onClosed = new EventEmitter();
 
 //	@Input() siteId:number;//this kind of binding can't work, we need to attack the value to an event
-	private _siteId:number;
-	@Input() set siteId( id:number ){
-		console.log("EditSiteComponent::setSiteId(): ", this._siteId );
-		this._siteId = id;
-		let site = this.siteService.getSiteById( this._siteId );
-		if( site ){
-			this.siteName = site.name;
-			this.maxDepth = site.maxDepth;
-		}
-
-	}
+//	private _siteId:number;
+//	@Input() set siteId( id:number ){
+//		console.log("EditSiteComponent::setSiteId(): ", this._siteId );
+//		this._siteId = id;
+//		let site = this.siteService.getSiteById( this._siteId );
+//		if( site ){
+//			this.siteName = site.name;
+//			this.maxDepth = site.maxDepth;
+//		}
+//
+//	}
+	siteId:number;
+	site:DiveSite;
 	siteName:string;
 	maxDepth:number;
 
-	constructor( private siteService:SiteMgmtService ){
+	constructor(
+		private siteService:SiteMgmtService
+		,private route:ActivatedRoute
+		,private router:Router
+	){
+
+		this.siteId		= this.route.snapshot.params["id"]
+		this.site 		= this.siteService.getSiteById( this.siteId );
+		this.siteName	= this.site.name;
+		this.maxDepth	= this.site.maxDepth;
 //		console.log("EditSiteComponent() siteId: ", this.siteId );
 		//on construction, this.siteId is a semi-meaningless value from the list view
 		//it won't have a meaningful value until a runtime click event "picks" the siteId value
@@ -37,13 +50,14 @@ export class EditSiteComponent {
 	}
 
 	doSave():void{
-		this.siteService.saveSite( {id: this._siteId, name: this.siteName, maxDepth: this.maxDepth} );
-		this.onClosed.emit(null);
+		this.siteService.saveSite( {id: this.siteId, name: this.siteName, maxDepth: this.maxDepth} );
+		this.router.navigate( [this.siteService.Paths.LIST] );
+//		this.onClosed.emit(null);
 	}
 
-	doCancel():void{
-		this.onClosed.emit(null);
-	}
+//	doCancel():void{
+//		this.onClosed.emit(null);
+//	}
 
 
 }//EditSiteComponent
