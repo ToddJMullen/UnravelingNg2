9f4da5f1b6c274e2e689930938cf7efd4d9965f9
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-09-26 21:07:12 -0400

    DeleteSiteComponent - implement Router utlities in looking up siteId from route & forwarding after user actions.

--- Chapter03/My03-01/app/delete-site.component.ts
+++ Chapter03/My03-01/app/delete-site.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
-import {OnChanges}	from "@angular/core";
+//import {Component, Input, Output, EventEmitter} from '@angular/core';
+//import {OnChanges}	from "@angular/core";
+import {Component} from '@angular/core';
+import { Router, ActivatedRoute }	from "@angular/router";
 
 //import {DiveSite} from './dive-site';//no longer needed since "those" are handled in their service
 import {SiteMgmtService}	from "./site-mgmt.service";
@@ -8,29 +10,40 @@ import {SiteMgmtService}	from "./site-mgmt.service";
   selector: 'delete-site-view',
   templateUrl: 'app/delete-site.template.html'
 })
-export class DeleteSiteComponent implements OnChanges {
-  @Input() siteId:number;
+export class DeleteSiteComponent{// implements OnChanges {
+//  @Input() siteId:number;
 //  @Output() onDeleted = new EventEmitter();
 //  @Output() onCancel = new EventEmitter();
-	@Output() onClosed = new EventEmitter();
+//	@Output() onClosed = new EventEmitter();
+	siteId:number;
 	siteName:string;
 //	maxDepth:number
 	
-	constructor( private siteService:SiteMgmtService ){}
-	
-	ngOnChanges(){
-		let site = this.siteService.getSiteById( this.siteId );
-		this.siteName = site.name;
-//		this.maxDepth = site.maxDepth;
+	constructor(
+		private siteService:SiteMgmtService
+		,private route:ActivatedRoute
+		,private router:Router
+	){
+		this.siteId = this.route.snapshot.params["id"];
+		this.siteName = this.siteService.getSiteById( this.siteId ).name;
 	}
 	
+//	ngOnChanges(){
+//		let site = this.siteService.getSiteById( this.siteId );
+//		this.siteName = site.name;
+////		this.maxDepth = site.maxDepth;
+//	}
+
 	doDelete(){
+		console.log("DeleteSiteComponent::doDelete()", this.siteId );
 		this.siteService.deleteSite(this.siteId);
-		this.onClosed.emit(null);
+		this.router.navigate([this.siteService.Paths.LIST]);//go back home
+//		this.onClosed.emit(null);
 	}
 	
 	doCancel(){
-		this.onClosed.emit(null);
+//		this.onClosed.emit(null);
+		this.router.navigate([this.siteService.Paths.LIST]);
 	}
 
 //  deleted() {
