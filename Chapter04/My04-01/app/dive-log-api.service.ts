bdc6e0a5c54b5448e53d8100fc2b012c345ebb02
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-09-28 20:19:23 -0400

    Remove author implementation of Ex 4-1 because copy/past is not learning. =D

--- Chapter04/My04-01/app/dive-log-api.service.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from '@angular/core';
-import {DiveLogEntry} from './dive-log-entry';
-
-@Injectable()
-export class DiveLogApi {
-  getDives() {
-    return DiveLogEntry.StockDives;
-  }
-}
