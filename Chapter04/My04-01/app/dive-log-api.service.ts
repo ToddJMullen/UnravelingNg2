1f1c6a34a7254d4a62572fde6850238c1f763610
Todd Mullen <ToddJMullen@gmail.com>
2017-09-29 07:47:25 -0400

    DiveLogApi, Comment on Brackets linting error about the returned Promise, import Http and get ready to convert to an actual backend call.

--- Chapter04/My04-01/app/dive-log-api.service.ts
+++ Chapter04/My04-01/app/dive-log-api.service.ts
@@ -1,4 +1,6 @@
 import {Injectable} from "@angular/core";
+import {Http}   from "@angular/http";
+import 'rxjs/add/operator/toPromise';
 
 import {DiveLogEntry} from "./dive-log-entry";
 
@@ -9,20 +11,24 @@ export class DiveLogApi{
 	static CALLS_TO_FAIL:number = 3;
 	static callsMade:number = 0;
 
+    private URI_DIVE_LOG_API = "http://unraveling-ng.azurewebsites.net/api/backendtest/dives";
+
 	//so far the only method will return the div logs
 	getDives():Promise<DiveLogEntry[]>{
 //		return DiveLogEntry.BaseDives;//old boredom stuff
-		return new Promise<DiveLogEntry[]>( (onResolve, onReject) => {
-			setTimeout( () => {
-
-				if( DiveLogApi.callsMade++ % DiveLogApi.CALLS_TO_FAIL == 0 ){
-					onReject(`${DiveLogApi.callsMade} calls to this servive is too many! Go away!! 8b`);
-				} else {
-					onResolve( DiveLogEntry.BaseDives );
-				}
-
-			}, DiveLogApi.MS_DELAY_API );
-		});
+//      Brackets is complaining about this promise saying:
+//      "Promise refers to a type, but is being used as a value here" (still works though)
+//		return new Promise<DiveLogEntry[]>( (onResolve, onReject) => {
+//			setTimeout( () => {
+//
+//				if( DiveLogApi.callsMade++ % DiveLogApi.CALLS_TO_FAIL == 0 ){
+//					onReject(`${DiveLogApi.callsMade} calls to this service is too many! Go away!! 8b`);
+//				} else {
+//					onResolve( DiveLogEntry.BaseDives );
+//				}
+//
+//			}, DiveLogApi.MS_DELAY_API );
+//		});
 	}
 
 }//DiveLogApi
