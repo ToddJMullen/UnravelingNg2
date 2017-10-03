96c601244678b42f6eab221332f7e8553dc956e9
Todd Mullen <ToddJMullen@gmail.com>
2017-10-03 19:54:51 -0400

    PlannerComponent - embed / use imported DivePlannerComponent & TourPlannerComponent in the template markup.

--- Chapter06/My06-01/app/planner/planner.component.ts
+++ Chapter06/My06-01/app/planner/planner.component.ts
@@ -6,6 +6,8 @@ import {Component} from '@angular/core';
     <div class="row">
       <div class="col-sm-12">
         <h2>Planner Component</h2>
+        <dive-planner></dive-planner>
+        <tour-planner></tour-planner>
       </div>
     </div>
   `,
