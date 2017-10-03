e0e62d2af8644cbe83759846986bc200d76f810d
Todd Mullen <ToddJMullen@gmail.com>
2017-10-03 19:08:01 -0400

    Create  basic (empty)TourPlannerComponent with a different Bg color.

---
+++ Chapter06/My06-01/app/planner/tour-planner.component.ts
@@ -0,0 +1,16 @@
+import {Component}  from "@angular/core";
+
+@Component({
+    selector: "tour-planner"
+    ,template: `
+<div class='col-xs-6'>
+<h3>Tour Planner</h3>
+</div>
+`
+    ,styles: [`
+.col-xs-6{
+background-color: #0ef;
+}
+`]
+})
+export class TourPlannerComponent{}
