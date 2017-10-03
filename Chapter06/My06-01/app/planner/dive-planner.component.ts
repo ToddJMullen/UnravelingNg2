45482c9e409938845205c9bfb81687e8a1c44611
Todd Mullen <ToddJMullen@gmail.com>
2017-10-03 19:10:38 -0400

    Create a DivePlannerComponent with it's own background color;

---
+++ Chapter06/My06-01/app/planner/dive-planner.component.ts
@@ -0,0 +1,18 @@
+import {Component}  from "@angular/core";
+
+import {BrowserModule} from "@angular/platform-browser";
+
+@Component({
+    selector: "dive-planner"
+    ,template: `
+<div class="col-xs-6">
+ <h3>Dive Planner</h3>
+</div>
+`
+    ,styles: [`
+.col-xs-6{
+background-color: #ef3;
+}
+`]
+})
+export class DivePlannerComponent{}
