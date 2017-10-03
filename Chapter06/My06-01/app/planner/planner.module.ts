a5705aa50e7ab67f394e46a2633ca8bbe99e1a59
Todd Mullen <ToddJMullen@gmail.com>
2017-10-03 19:19:27 -0400

    Integrate DivePlannerComponent & TourPlannerComponent into PlannerModule & export them from

--- Chapter06/My06-01/app/planner/planner.module.ts
+++ Chapter06/My06-01/app/planner/planner.module.ts
@@ -2,10 +2,20 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {PlannerComponent} from './planner.component';
+import {DivePlannerComponent}   from "./dive-planner.component";
+import {TourPlannerCommponent}  from "./tour-planner.component";
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [PlannerComponent],//Declaration of availability within the module
-  exports: [PlannerComponent]//declaration that it's to be available to other modules inporting this one
+  declarations: [
+      PlannerComponent
+      ,DivePlannerComponent
+      ,TourPlannerCommponent
+  ],//Declaration of availability within the module
+  exports: [
+      PlannerComponent
+      ,DivePlannerComponent
+      ,TourPlannerCommponent
+  ]//declaration that it's to be available to other modules inporting this one
 })
 export class PlannerModule { }
