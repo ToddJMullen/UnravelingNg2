54ff774afc01c9e0ed09f33f1be71ec3c509bce6
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-03 21:39:35 -0400

    Add new LoogBook & Buddy modules & components to AppModule & look at the pretty zoned colors! xD

--- Chapter06/My06-01/app/app.component.ts
+++ Chapter06/My06-01/app/app.component.ts
@@ -6,6 +6,8 @@ import {Component} from '@angular/core';
     <div class="container-fluid">
       <h1>Younderwater Portal</h1>
       <yw-planner></yw-planner><!-- <= imported from the planner module/component -->
+      <log-book></log-book>
+      <buddy-comp></buddy-comp>
     </div>
   `
 })
--- Chapter06/My06-01/app/app.module.ts
+++ Chapter06/My06-01/app/app.module.ts
@@ -3,11 +3,15 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {PlannerModule} from './planner/planner.module';//"phyysical" import
+import {LogBookModule} from "./logbook/log-book.module";
+import {BuddyModule} from "./buddies/buddy.module";
 
 @NgModule({
   imports: [
-    BrowserModule,
-    PlannerModule//logical import, imports all the exported classes of PlannerModule into the AppModule scope
+    BrowserModule
+    ,PlannerModule//logical import, imports all the exported classes of PlannerModule into the AppModule scope
+    ,BuddyModule
+    ,LogBookModule
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
