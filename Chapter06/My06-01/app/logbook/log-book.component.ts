c17eab9f5e63e924e14f6fa56f717708aff6195e
Todd Mullen <ToddJMullen@gmail.com>
2017-10-04 22:47:58 -0400

    Import Giga BorderDirective & add comment

--- Chapter06/My06-01/app/logbook/log-book.component.ts
+++ Chapter06/My06-01/app/logbook/log-book.component.ts
@@ -1,11 +1,15 @@
 import {Component} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 
+import {BorderDirective} from "../thirdparty/giga/border.directive";
+
 @Component({
   selector: "log-book"
   ,template: `
-<div class="col-xs-6">
+<div class="col-xs-6"
+ giga-border="outset" giga-border-px="2" giga-border-color="silver">
 <h3>Log Book</h3>
+<p>Has Giga BorderDirective applied & imported, but doesn't reset on mouseleave</p>
 </div>
 `
   ,styles: [`
