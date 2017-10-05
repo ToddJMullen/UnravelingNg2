311f7462edb2cde8b817608f51b2133dc9f4f8a5
Todd Mullen <ToddJMullen@gmail.com>
2017-10-04 23:47:07 -0400

    buddy.module.ts - Add imports for both UtilityModules & alias the Giga one as/in GigaUtils

--- Chapter06/My06-01/app/buddies/buddy.module.ts
+++ Chapter06/My06-01/app/buddies/buddy.module.ts
@@ -1,10 +1,17 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 
+import {UtilityModule} from "../thirdparty/acme/utility.module";
+import {UtilityModule as GigaUtils} from "../thirdparty/giga/utility.module";
+
 import {BuddyComponent} from "./buddy.component";
 
 @NgModule({
-  imports: [BrowserModule]
+  imports: [
+      BrowserModule
+      ,UtilityModule
+      ,GigaUtils
+  ]
   ,declarations: [BuddyComponent]
   ,exports: [BuddyComponent]
 })
