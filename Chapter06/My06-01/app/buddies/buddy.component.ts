8dd141fb713d2b343bca6a76567d37da4caf58a1
Todd Mullen <ToddJMullen@gmail.com>
2017-10-04 23:46:14 -0400

    buddy.component.ts - Remove comment about border nnot resetting.

--- Chapter06/My06-01/app/buddies/buddy.component.ts
+++ Chapter06/My06-01/app/buddies/buddy.component.ts
@@ -1,11 +1,19 @@
 import {Component} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 
+import {HighlightDirective} from "../thirdparty/acme/highlight.directive";
+import {BorderDirective} from "../thirdparty/giga/border.directive";
+
+
 @Component({
   selector: "buddy-comp"
   ,template: `
-<div class="col-xs-6">
+<div class="col-xs-6"
+    acme-highlight="#37f" acme-cursor="help"
+    giga-border="inset" giga-border-px="3" giga-border-color="#000">
 <h3>Buddy Component</h3>
+<p>Has Giga BorderDirective applied &amp; imported</p>
+<p>Has the HighlightDirective applied &amp; imported</p>
 </div>
 `
   ,styles: [`
