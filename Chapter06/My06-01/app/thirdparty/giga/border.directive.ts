317336a1457df364ed321de45ff48d542252943a
Todd Mullen <ToddJMullen@gmail.com>
2017-10-04 22:24:30 -0400

    border.directive.ts - Initial implementation of the BorderDirective

--- Chapter06/My06-01/app/thirdparty/giga/border.directive.ts
+++ Chapter06/My06-01/app/thirdparty/giga/border.directive.ts
@@ -1,6 +1,43 @@
 import {Directive, ElementRef, HostListener} from "@angular/core";
+import {Input}  from "@angular/core";
+
 
 @Directive({
-  selector: "[addBorder]"
+  selector: "[giga-border]"
 })
-export class BorderDirective{}
+export class BorderDirective{
+
+    @Input("giga-border")
+    type:string;
+
+    @Input("giga-border-px")
+    px:number;
+
+    @Input("giga-border-color")
+    color:string;
+
+    constructor( private elem:ElementRef ){
+        console.log("BorderDirective()", this.elem );
+    }
+
+    @HostListener('mouseenter')
+    onMouseEnter(){
+        this.applyStyle( this.type, this.color, this.px );
+    }
+
+    @HostListener("mouseleave")
+    onMouseLeave(){
+        this.applyStyle( null, null, null );
+    }
+
+
+    applyStyle( type, color, px ){
+        console.log("BorderDirective::applyStyle()", type, color, px );
+        let style = this.elem.nativeElement.style;
+        style.borderColor = this.color;
+        style.borderWidth = this.px + "px";
+        style.borderStyle = this.type;
+    }
+
+
+}//BorderDirective
