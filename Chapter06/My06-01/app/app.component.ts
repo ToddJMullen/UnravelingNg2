6f83b79f376e52d7cb4eb45d0ad40d0c7c910ec0
Todd Mullen <ToddJMullen@gmail.com>
2017-10-06 08:19:23 -0400

    AppComponent - Adjust template indentation

--- Chapter06/My06-01/app/app.component.ts
+++ Chapter06/My06-01/app/app.component.ts
@@ -4,11 +4,11 @@ import {Component} from '@angular/core';
   selector: 'yw-app',
   template: `
     <div class="container-fluid">
-      <h1>Younderwater Portal</h1>
-      <yw-planner></yw-planner><!-- <= imported from the planner module/component -->
-      <log-book></log-book>
-      <buddy-comp></buddy-comp>
-      <inventory></inventory>
+        <h1>Younderwater Portal</h1>
+        <yw-planner></yw-planner>
+        <log-book></log-book>
+        <buddy-comp></buddy-comp>
+        <inventory></inventory>
     </div>
   `
 })
