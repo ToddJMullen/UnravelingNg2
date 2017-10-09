692584346812af8532e82d32ffcfe493ef5310c6
Todd Mullen <ToddJMullen@gmail.com>
2017-10-09 18:41:49 -0400

    AppModule - Change tracer DI injection from manual instantiation to using an opaque token ???

--- Chapter06/My06-06/app/app.module.ts
+++ Chapter06/My06-06/app/app.module.ts
@@ -7,9 +7,10 @@ import {DiverComponent} from './diver.component';
 // import {InventoryService} from './inventory.service';
 //import {InventoryService} from "./tracing-inventory.service"
 import {TraceService, AdvancedTraceService} from "./trace.service";
+import {OPAQUE_VERSION} from"./opaque-version.token"
 
-let myInstance = new AdvancedTraceService();
-myInstance.info = "(my manual instance)"
+//let myInstance = new AdvancedTraceService();
+//myInstance.info = "(my manual instance)"
 
 
 @NgModule({
@@ -22,8 +23,9 @@ myInstance.info = "(my manual instance)"
    ,providers: [
 //	   InventoryService
 //	   TraceService//Provide one instance for the whole application
-//	   AdvancedTraceService
-       ,{provide:AdvancedTraceService, useValue: myInstance}
+	   AdvancedTraceService
+       ,{provide: OPAQUE_VERSION, useValue: "v0.12"}
+//       ,{provide: AdvancedTraceService, useValue: myInstance}
 	   ,{provide: TraceService, useExisting: AdvancedTraceService}
    ]
   ,bootstrap: [AppComponent]
