4fea44e83a0245fc3641cee8a8db130598dd6b0d
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-08 20:17:54 -0400

    Replace existing InventoryService implementation with new optional tracing implementation using an alias as the same name.

--- Chapter06/My06-06/app/diver.component.ts
+++ Chapter06/My06-06/app/diver.component.ts
@@ -2,14 +2,21 @@ import {Component} from '@angular/core';
 
 // import {InventoryService} from "./inventory.service";
 // import {IInventory} from "./tracing-inventory.service";
-import {InventoryService, TracingInventoryService} from './tracing-inventory.service';
+//import {InventoryService, TracingInventoryService} from './tracing-inventory.service';
+import {InventoryService} from './tracing-inventory.service';
+import {InventoryPlusTracingService} from "./inventory-plus-tracing.service";
 
 @Component({
   selector: 'diver'
   ,providers: [
 //    InventoryService
-    {provide: InventoryService
-    ,useClass:TracingInventoryService}
+//    {provide: InventoryService
+//    ,useClass:TracingInventoryService}
+//	  ,InventoryPlusTracingService//Provide one inventory for each diver
+	  //I don't want to use that long name, so I'll alias it
+	  ,{provide: InventoryService
+	  	,useClass: InventoryPlusTracingService
+	  }
   ]
   ,template: `
     <div class="col-sm-3">
