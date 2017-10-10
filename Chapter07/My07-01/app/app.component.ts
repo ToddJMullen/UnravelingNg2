222b384b5e004fc96138fdaf9dce8e1f169bfaf9
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-09 22:52:50 -0400

    Not much to do. Just duplicated some of the components in index.html and add comments a to the component instance markup.

--- Chapter07/My07-01/app/app.component.ts
+++ Chapter07/My07-01/app/app.component.ts
@@ -3,24 +3,32 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'yw-app',
   template: `
+  <!-- start of the parent app's content -->
     <div class="container-fluid">
       <h2>AppComponent</h2>
       <div class="container-fluid">
         <div class="row">
           <div class="col-sm-8">
+            <!-- demarcating where the/a tagline component should go -->
+            <yw-tagline></yw-tagline>
             <yw-tagline></yw-tagline>
           </div>
           <div class="col-sm-4">
+            <!-- a search component instance -->
             <yw-search></yw-search>
+            <yw-tagline></yw-tagline>
           </div>
         </div>
         <div class="row">
           <div class="col-sm-12">
+          <!-- a list -->
             <yw-list></yw-list>
           </div>
         </div>
         <div class="row">
           <div class="col-sm-12">
+            <yw-list></yw-list>
+          <!-- and a footer comp -->
             <yw-footer></yw-footer>
           </div>
         </div>
