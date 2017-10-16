6141a270e6ae5936e4be82b1bebc970631e3c4d5
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-15 22:31:20 -0400

    AcmeComponent, Add second ng-content tag to template Wrap both ng-content tags in divs with borders to outline them.

--- Chapter07/My07-09/app/acme.component.ts
+++ Chapter07/My07-09/app/acme.component.ts
@@ -4,12 +4,18 @@ import {Component} from '@angular/core';
   selector: 'acme',
   template: `
     <h2>This is the AcmeComponent template h2</h2>
-	<p>This is a p followed by the ng-content tag...</p>
+	<div class='box'>This is a div followed by the ng-content tag...
     <ng-content></ng-content>
-	<p>...This is a p after the ng-content tag</p>
-    <ng-content></ng-content>
-	<p style='font-size:140%;'>...This is a p after a <b>second</b> ng-content tag</p>
+	...This is the rest of div after the ng-content tag</div>
+	<div class='box'>
+	This is before a secong ng-content tag
+	<ng-content></ng-content>
+	...This is after the <b>second</b> ng-content tag</div>
+	<p>...So apparently it's only rendered once &amp; in the place of the last occurence.</p>
   `
+  ,styles: [`
+	.box{border:1px solid red; margin: 5px; padding: 5px;}
+`]
 })
 export class AcmeComponent {
 }
