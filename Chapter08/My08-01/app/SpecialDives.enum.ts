fba7deff5c14f1dabad19190392e6e327dc9933a
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-21 21:29:22 -0400

    Declarw SpecialDivesEnum with readonly properties

---
+++ Chapter08/My08-01/app/SpecialDives.enum.ts
@@ -0,0 +1,6 @@
+export class SpecialDivesEnum{
+	static readonly NIGHT_DIVE		= "Night Dive";
+	static readonly CAVE_DIVE		= "Cave Dive";
+	static readonly DEEP_DIVE		= "Deep Dive";
+	static readonly SAMMICH_DIVE	= "Sammich Dive!";
+}
