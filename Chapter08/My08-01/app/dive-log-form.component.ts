125cffe4ae6a6f7e81c029d2b6fae1dce6565801
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-21 21:32:06 -0400

    Add entry: DiveLogEntry to DiveLogFormComponent to encapsulate and bundle the form properties. Import SpecialDivesEnum to gaurantee spcial property value accuracy.

--- Chapter08/My08-01/app/dive-log-form.component.ts
+++ Chapter08/My08-01/app/dive-log-form.component.ts
@@ -1,13 +1,22 @@
 import {Component} from '@angular/core';
 
+import {DiveLogEntry} from "./dive-log-entry.iface";
+import {SpecialDivesEnum} from "./SpecialDives.enum";
+
 @Component({
   selector: 'dive-log-form',
   templateUrl: 'app/dive-log-form.template.html'
 })
 export class DiveLogFormComponent {
-  specialDives = [
-    "Night dive",
-    "Deep dive",
-    "Cave dive"
-  ]
+
+
+	entry: DiveLogEntry = {
+		site: "Barnes & Noble"
+		,location: "Dale Mabry in Tampa"
+		,depth: 1
+		,time: 150
+		,isFavorite: true
+		,special: SpecialDivesEnum.DEEP_DIVE
+	}
+
 }
