ff718192563e9488bdcb403c7a0c9e2b07e3a46b
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-24 21:50:52 -0400

    Manually re-implement AppModule & AppRoutes

--- Chapter09/My09-01/app/app.module.ts
+++ Chapter09/My09-01/app/app.module.ts
@@ -1,24 +1,30 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
+import {NgModule}		from "@angular/core";
+import {BrowserModule}	from "@angular/platform-browser"
 
-import {WelcomeModule} from './welcome/welcome.module';
-import {DiveLogModule} from './logs/dive-log.module';
-import {SitesModule} from './sites/sites.module';
+import {AppComponent}	from "./app.component";
 
-import {routingModule} from './app.routes';
+import {WelcomeModule}	from "./welcome/welcome.module";
+import {DiveLogModule}	from "./logs/dive-log.module";
+import {SitesModule}	from "./sites/sites.module";
+
+
+import {routingModule} from "./app.routes";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    WelcomeModule,
-    DiveLogModule,
-    SitesModule,
-    routingModule
-  ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [AppComponent]
+	imports: [
+		BrowserModule
+		,WelcomeModule
+		,DiveLogModule
+		,SitesModule
+		,routingModule
+	]
+	,declarations: [
+		AppComponent
+	]
+//	,providers: [
+//
+//	]
+	,bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule{}
+
--- Chapter09/My09-01/app/app.routes.ts
+++ Chapter09/My09-01/app/app.routes.ts
@@ -1,12 +1,16 @@
-import {Routes, RouterModule} from '@angular/router';
-import {WelcomeComponent} from './welcome/welcome.component';
-import {DiveLogComponent} from './logs/dive-log.component';
-import {SiteListComponent} from './sites/site-list.component';
+import {Routes, RouterModule}	from "@angular/router";
+import {WelcomeComponent}		from "./welcome/welcome.component";
+import {SiteListComponent}		from "./sites/site-list.component";
+import {DiveLogComponent}		from "./logs/dive-log.component";
 
-const routes: Routes = [
-  { path: 'divelogs', component: DiveLogComponent },
-  { path: 'sites', component: SiteListComponent },
-  { path: '', pathMatch: 'full', component: WelcomeComponent }
+
+const routeAry:Routes = [
+	{path: "divelogs", component: DiveLogComponent }
+	,{path: "sites", component: SiteListComponent }
+	,{path: "", pathMatch: "full", component: WelcomeComponent }
 ];
 
-export const routingModule = RouterModule.forRoot(routes);
+export const routingModule = RouterModule.forRoot( routeAry );
+
+
+
