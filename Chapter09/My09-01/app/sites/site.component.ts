b9a3fba6fae2672740420a5893e96a6df3b678e1
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-25 21:26:20 -0400

    Declate SitesComponent as host for site routes.

---
+++ Chapter09/My09-01/app/sites/site.component.ts
@@ -0,0 +1,7 @@
+import {Component} from "@angular/core";
+
+@Component({
+//	selector:"na"//not needed or instantiated, this component is only a host
+	template: `<router-outlet></router-outlet>`
+})
+export class SitesComponent{}
