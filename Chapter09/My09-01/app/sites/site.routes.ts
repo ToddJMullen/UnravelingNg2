90c9672378fadfed1fd935bbe3da95a8d3e08f36
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-25 21:27:54 -0400

    Define siteRoutes and siteRoutingModule to handle the /sites/* sub-routes

---
+++ Chapter09/My09-01/app/sites/site.routes.ts
@@ -0,0 +1,23 @@
+import {Routes, RouterModule} from "@angular/router";
+import {SitesComponent} from "./site.component";
+import {SiteListComponent} from "./site-list.component";
+import {AddSiteComponent} from "./add-site.component";
+import {EditSiteComponent} from "./edit-site.component";
+import {DeleteSiteComponent} from "./delete-site.component";
+
+export const siteRoutes: Routes = [
+	{
+		path: "sites"//base for these paths
+		,component: SitesComponent//the host we created for just this purpose
+		,children: [
+			{path: "", component: SiteListComponent }
+			,{path: "add", component: AddSiteComponent }
+			,{path: "edit/:id", component: EditSiteComponent }
+			,{path: "drop/:id", component: DeleteSiteComponent }
+		]
+	}
+]//siteRoutes
+
+export const siteRoutingModule = RouterModule.forChild( siteRoutes );
+
+
