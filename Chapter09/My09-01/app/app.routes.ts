d07d7a83a8475625d657cb43d8a9223f745378eb
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-26 23:07:35 -0400

    app.routes.ts, Add login/auth guard to app routing

--- Chapter09/My09-01/app/app.routes.ts
+++ Chapter09/My09-01/app/app.routes.ts
@@ -5,13 +5,24 @@ import {DiveLogComponent}		from "./logs/dive-log.component";
 
 //add subroute module(s)
 import {siteRoutes}				from "./sites/site.routes";
+//add login elements
+import {UserAuthService}		from "./login/user-auth.service"
+import {LoggedInGuard} 			from "./login/logged-in.guard"
+import {LoginComponent}			from "./login/login.component";
 
 
 const routeAry:Routes = [
-	{path: "divelogs", component: DiveLogComponent }
+	{path: "divelogs", component: DiveLogComponent
+		,canActivate: [LoggedInGuard]}
+	,{path: "login", component: LoginComponent }
 	,{path: "", pathMatch: "full", component: WelcomeComponent }
 	, ...siteRoutes
 	//replace single sites/ path with all site sub-routes
 ];
 
 export const routingModule = RouterModule.forRoot( routeAry );
+
+export const routingProviders	= [
+	LoggedInGuard
+	,UserAuthService
+]
