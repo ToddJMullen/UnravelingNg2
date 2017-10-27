5bc5034f8ac8f21754ebe0ccb35f28a37192d635
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-27 09:47:03 -0400

    Add logout button / feature to app nav menu.

--- Chapter09/My09-01/app/app.component.ts
+++ Chapter09/My09-01/app/app.component.ts
@@ -1,8 +1,16 @@
 import {Component} from '@angular/core';
 
+import {UserAuthService} from "./login/user-auth.service";
+
+
 @Component({
   selector: 'yw-app',
   templateUrl: 'app/app.template.html'
 })
 export class AppComponent {
+
+
+	constructor(
+		private authService: UserAuthService
+	){}
 }
