e687f2dee636a70bf7b3d268db32ae06d6b6bf03
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-27 20:45:36 -0400

    AddSiteComponent & EditSiteComponent, implement DeactivatableComponent to guard against lost changes

--- Chapter09/My09-01/app/sites/add-site.component.ts
+++ Chapter09/My09-01/app/sites/add-site.component.ts
@@ -3,13 +3,17 @@ import {Router} from '@angular/router';
 import {ActivatedRoute} from "@angular/router";
 
 import {SiteManagementService} from './site-management.service'
+import {DeactivatableComponent} from "../routing/can-deactivate.guard";
 
 @Component({
   selector: 'add-site-view',
   templateUrl: 'app/sites/add-site.template.html'
 })
-export class AddSiteComponent {
-  siteName: string;
+export class AddSiteComponent
+	implements DeactivatableComponent{
+
+	siteName: string = "";
+	ogSiteName:string = "";
 
   constructor(
 		private siteService: SiteManagementService
@@ -19,7 +23,14 @@ export class AddSiteComponent {
 
   add() {
     this.siteService.addSite({id: 0, name:this.siteName});
+	  this.ogSiteName = this.siteName;
 //    this.router.navigate(['/list']);
 	  this.router.navigate(['..'], {relativeTo: this.currentRoute});
-  }
-}
+  }//add()
+
+	//the check if they can leave is to see if they have entered anything for the site name
+	allowLeave = () => this.siteName == this.ogSiteName;
+	confirmPrompt = () => "You not finished!";
+
+
+}//AddSiteComponent()
--- Chapter09/My09-01/app/sites/edit-site.component.ts
+++ Chapter09/My09-01/app/sites/edit-site.component.ts
@@ -2,15 +2,21 @@ import {Component} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 
 import {SiteManagementService} from './site-management.service'
+import {DeactivatableComponent} from "../routing/can-deactivate.guard";
+
 
 @Component({
   selector: 'edit-site-view',
   templateUrl: 'app/sites/edit-site.template.html'
 })
-export class EditSiteComponent {
+export class EditSiteComponent
+	implements DeactivatableComponent{
+
   siteId: number;
   siteName: string;
   private parSub: any;
+
+	ogSiteName:string = "";
 
   constructor(
     private siteService: SiteManagementService,
@@ -18,12 +24,21 @@ export class EditSiteComponent {
     private router: Router
   ) {
     this.siteId = this.route.snapshot.params['id'];
-    this.siteName = this.siteService
-      .getSiteById(this.siteId).name;
+    this.siteName = this.siteService.getSiteById(this.siteId).name;
+	this.ogSiteName = this.siteName;
   }
 
   save() {
     this.siteService.saveSite({id: this.siteId, name:this.siteName});
+	this.ogSiteName = this.siteName;
     this.router.navigate(['../..'], {relativeTo: this.route});
   }
+
+
+	allowLeave(){
+		return this.siteName == this.ogSiteName;
+	}
+	confirmPrompt(){
+		"You sure? You forgot to save!"
+	}
 }
