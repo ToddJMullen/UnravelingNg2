0f28a84e951515bc06f553e83510e8f6ce138d1e
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-28 23:25:05 -0400

    Define DiveDataPipe Pipe to  format dive depth & time information using the 'diveData' pipe flag.

---
+++ Chapter10/My10-01/app/dive-data.pipe.ts
@@ -0,0 +1,15 @@
+import {Pipe, PipeTransform} from "@angular/core";
+
+import {DiveLogEntry} from "./dive-log-entry";
+
+@Pipe({name: "diveData"})
+export class DiveDataPipe
+    implements PipeTransform{
+    transform( dive:DiveLogEntry, separator:any = " | " ):string{
+        if( dive && dive.depth && dive.time ){
+            return `${dive.depth}&apos;${separator}${dive.time} m`
+        }
+        return "(no data / data missing)";
+    }
+}
+
