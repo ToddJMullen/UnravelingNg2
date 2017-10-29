a7efb840d192fdd98a02f70c48811064fbc66a58
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-28 21:49:01 -0400

    dive-log.component, Add avgTime() & avgDepth() methods.

--- Chapter10/My10-01/app/dive-log.component.ts
+++ Chapter10/My10-01/app/dive-log.component.ts
@@ -7,4 +7,24 @@ import {DiveLogEntry} from './dive-log-entry';
 })
 export class DiveLogComponent {
   public dives = DiveLogEntry.StockDives;
+
+
+  avgTime() {
+    let sum = 0.0
+    ,numDives = this.dives.length;
+    for (var i = 0; i < numDives; i++) {
+      sum += this.dives[i].time;
+    }
+    return sum / numDives;
+  }
+      
+  avgDepth() {
+    var sum = 0.0
+    ,numDives = this.dives.length;
+    for (var i = 0; i < numDives; i++) {
+      sum += this.dives[i].depth;
+    }
+    return sum / numDives;
+  }
+
 }
