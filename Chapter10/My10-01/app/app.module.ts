58af64f959785278c1462313887975b351b61030
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-10-28 21:53:10 -0400

    Add NumberListComponent  to the view

--- Chapter10/My10-01/app/app.module.ts
+++ Chapter10/My10-01/app/app.module.ts
@@ -2,10 +2,13 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {DiveLogComponent} from './dive-log.component';
+import {NumberListComponent} from "./number-list.component";
+
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [DiveLogComponent],
+  declarations: [DiveLogComponent
+,NumberListComponent],
   bootstrap: [DiveLogComponent]
 })
 export class AppModule { }
